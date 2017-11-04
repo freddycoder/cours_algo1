e4f1e773bd563a7686c4428a8821c764443f66d2
Frédéric Jacques <gary.bob88@gmail.com>
2017-11-04 19:00:47 -0400

    Update TP-1_no2.py

--- TP-1_no2.py
+++ TP-1_no2.py
@@ -6,7 +6,7 @@ print("------------------------------------------------\n")
 nb = -1
 somme = 0
 
-while nb <= 0:
+while nb < 0:
   nb = int(input("Entre un nombre plus grand ou egual zero\n"))
   if nb < 0:
     print("Vous avez entrer un nombre negatif")
