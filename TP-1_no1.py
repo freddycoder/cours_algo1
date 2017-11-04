79b9a4000ee25a15f74aba5c8156111c75e2f703
Frédéric Jacques <gary.bob88@gmail.com>
2017-11-04 17:58:31 -0400

    Update TP-1_no1.py

--- TP-1_no1.py
+++ TP-1_no1.py
@@ -3,45 +3,30 @@ print("------------- Calculateur de prix --------------")
 print("------------------------------------------------\n")
 
 # Assignation des variable
-nombre_etiquette_grise = 0
-nombre_etiquette_blanche = 0
 prix_etiquette_blanche = 1.25
 prix_etiquette_grise = 1.50
 prix_de_livraison = 0.05
-livraison = False
+livraison = ""
 
 # Demande d'information à l'utilisateur
 nombre_etiquette_blanche = int(input("Entrée le nombre d'étiquette blanche : "))
 nombre_etiquette_grise = int(input("Entrée le nombre d'étiquette grisse : "))
 
-# Vérifier si l'utilsateur n'aurait pas entré des nombre négatif
-if nombre_etiquette_blanche < 0 or nombre_etiquette_grise < 0:
-  print("Vous ne pouvez pas entrée de nombre négatif. Le programme va maintenant quitté. Tapez sur entrer.")
-  input()
-  quit()
-
-# Récolter l'informaion par rapport au shipping
-while True:
-  livraison = str(input("Livrer les étiquettes? (o/n): "))
-  if livraison == "o":
-    livraison = True
-    break
-  elif livraison == "n":
-    livraison = False
-    break
+# Vérifier si l'utilsateur à rentrée des données valide.
+if nombre_etiquette_blanche > 0 or nombre_etiquette_grise > 0:
+  # Récolter l'informaion par rapport au shipping
+  while livraison != "n" and livraison != "o":
+    livraison = input("Livrer les étiquettes? (o/n): ")
+  # Calculer le sous-total et le total
+  nb_etiquette_total = nombre_etiquette_blanche + nombre_etiquette_grise
+  if livraison == True:
+    sous_total = nombre_etiquette_blanche * prix_etiquette_blanche + nombre_etiquette_grise * prix_etiquette_grise + prix_de_livraison * nb_etiquette_total
   else:
-    print("Vous avez entrée un caractère non valide pour la livraison.")
+    sous_total = nombre_etiquette_blanche * prix_etiquette_blanche + nombre_etiquette_grise * prix_etiquette_grise
+  print("\nSous-total : " + str(sous_total) + "\n" + "Total : " + str(round(sous_total*1.15, 2)))
 
-# Calculer le sous-total et le total
-nb_etiquette_total = nombre_etiquette_blanche + nombre_etiquette_grise
-if livraison == True:
-  sous_total = nombre_etiquette_blanche * prix_etiquette_blanche + nombre_etiquette_grise * prix_etiquette_grise + prix_de_livraison * nb_etiquette_total
 else:
-  sous_total = nombre_etiquette_blanche * prix_etiquette_blanche + nombre_etiquette_grise * prix_etiquette_grise + prix_de_livraison * nb_etiquette_total
-
-print("Sous-total : " + str(sous_total) + "\n" + "Total : " + str(round(sous_total*1.15, 2)))
-
-
+  print("Vous ne pouvez pas entrée de nombre négatif. Le programme va maintenant terminer.")
+  
 # Fin
-print("Merci d'avoi utiliser le calculateur de prix!")
-input("Tapez sur entré pour quitté")
+input("Merci d'avoi utiliser le calculateur de prix!\nTapez sur entré pour quitté")
