b2112a55cdda9dd1024c12a35f93a48f306423d5
Frédéric Jacques <gary.bob88@gmail.com>
2017-11-04 18:07:26 -0400

    Update TP-1_no2.py

--- TP-1_no2.py
+++ TP-1_no2.py
@@ -4,11 +4,18 @@ print("------------------------------------------------\n")
 
 # Assignation des variable
 nb = -1
-while nb < 0:
+while nb <= 0:
   nb = int(input("Entre un nombre plus grand ou egual zero\n"))
   if nb < 0:
     print("Vous avez entrer un nombre negatif")
 
+# La somme produit par une boucle
+somme = 0
+for i in range(1, nb+1):
+  somme = somme + i
+print("La somme est : " + str(somme))
+
+# La somme produit par une équation rapide à résoudre pour le processeur
 print("La somme est : " + str(int(nb*(nb+1)/2)))
 
 input("Tapez enter pour quitter")
