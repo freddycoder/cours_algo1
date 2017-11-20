3a84c7e0fd03e6d8845d562ef0c4c6e93da80783
Frédéric Jacques <gary.bob88@gmail.com>
2017-11-19 19:56:00 -0500

    Update TP-1_no1.py

--- TP-1_no1.py
+++ TP-1_no1.py
@@ -9,8 +9,8 @@ prix_de_livraison = 0.05
 livraison = ""
 
 # Demande d'information à l'utilisateur
-nombre_etiquette_blanche = int(input("Entrée le nombre d'étiquette blanche : "))
-nombre_etiquette_grise = int(input("Entrée le nombre d'étiquette grisse : "))
+nombre_etiquette_blanche = int(input("Entrer le nombre d'étiquettes blanches : "))
+nombre_etiquette_grise = int(input("Entrer le nombre d'étiquettes grises : "))
 
 # Vérifier si l'utilsateur à rentrée des données valide.
 if nombre_etiquette_blanche > 0 and nombre_etiquette_grise > 0:
@@ -18,7 +18,7 @@ if nombre_etiquette_blanche > 0 and nombre_etiquette_grise > 0:
   while livraison != "n" and livraison != "o":
     livraison = input("Livrer les étiquettes? (o/n): ")
   # Calculer le sous-total et le total
-  if livraison == True:
+  if livraison == "o":
     nb_etiquette_total = nombre_etiquette_blanche + nombre_etiquette_grise
     sous_total = nombre_etiquette_blanche * prix_etiquette_blanche + nombre_etiquette_grise * prix_etiquette_grise + prix_de_livraison * nb_etiquette_total
   else:
@@ -26,7 +26,7 @@ if nombre_etiquette_blanche > 0 and nombre_etiquette_grise > 0:
   print("\nSous-total : " + str(sous_total) + "\n" + "Total : " + str(round(sous_total*1.15, 2)))
 
 else:
-  print("Vous ne pouvez pas entrée de nombre négatif. Le programme va maintenant terminer.")
+  print("Vous ne pouvez pas entrer de nombre négatif. Le programme va maintenant terminer.")
   
 # Fin
-input("Merci d'avoi utiliser le calculateur de prix!\nTapez sur entré pour quitté")
+input("Merci d'avoir utilisé le calculateur de prix!\nTapez sur entrée pour quitter le programme.")
