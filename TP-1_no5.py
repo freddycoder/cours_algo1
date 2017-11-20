c6e7c335fe407b0c804a2512c999afc6cb646563
Frédéric Jacques <gary.bob88@gmail.com>
2017-11-19 20:16:41 -0500

    Update TP-1_no5.py

--- TP-1_no5.py
+++ TP-1_no5.py
@@ -2,13 +2,13 @@ print("------------------------------------------------")
 print("----------------- TP-1 - no.5 ------------------")
 print("------------------------------------------------\n")
 
-nb_jour_mois = int(input("Entrer le nombre de jour dans le mois. Doit être entre 28 et 31\n"))
-premier_jour_semaine = int(input("Enter quel est le premier jour de la semaine. Exemple 1 pour dimanche, 2 pour lundi... et 7 pour samedi\n"))
+nb_jour_mois = int(input("Entrer le nombre de jour dans le mois. Doit être entre 28 et 31 : "))
+premier_jour_semaine = int(input("Enter quel est le premier jour de la semaine. Exemple 1 pour dimanche, 2 pour lundi... et 7 pour samedi : "))
 compteur = 0
 jour = 1
 premiere_fois = True
 
-if nb_jour_mois <= 31 and nb_jour_mois >= 28 and premier_jour_semaine >= 0 and premier_jour_semaine <= 7:
+if nb_jour_mois <= 31 and nb_jour_mois >= 28 and premier_jour_semaine >= 1 and premier_jour_semaine <= 7:
   while jour <= nb_jour_mois:
     if premiere_fois == False:
       if compteur == 7:
@@ -33,4 +33,4 @@ if nb_jour_mois <= 31 and nb_jour_mois >= 28 and premier_jour_semaine >= 0 and p
       print()
        
 else:
-  input("Le programme va quitter car vous avez entrer des données non valide. \nAppuyer sur entrée pour sortire")
+  input("Le programme va quitter car vous avez entré des données non valides.\nAppuyer sur entrée pour sortir.")
